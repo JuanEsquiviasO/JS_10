09a1dfa6bde7cac2e9c0c33b26e623b7064b181a
Juan Esquivias <juanesquiviasotazu@gmail.com>
2018-02-26 14:11:12 -0500

    first program with JS

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-document.getElementById('app').innerHTML = 'Hi world';
+let name = prompt('What is your name?')
+let age = prompt ('How old are you?')
+
+document.getElementById('app').innerHTML = `Welcome ${name} , you have ${age} years`
