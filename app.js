510f22cb6f72d156ce75515180f41623b6fad059
Juan Esquivias <juanesquiviasotazu@gmail.com>
2018-02-26 16:28:16 -0500

    variables  with let and const

--- app.js
+++ app.js
@@ -18,11 +18,25 @@
 // console.log(dair)
 // console.log(dair1)
 
-var dias99 = 99
-console.log(dias99)
+// var dias99 = 99
+// console.log(dias99)
 
-var firstName = 'Roy'
-var first_name = 'Roy'
-var FirstName = 'Roy'
-var firstname = 'Roy'
-var first_name = 'Roy'
+// var firstName = 'Roy'
+// var first_name = 'Roy'
+// var FirstName = 'Roy'
+// var firstname = 'Roy'
+// var first_name = 'Roy'
+
+
+// let name = 'Ben'
+// let surname = 'Dixon'
+// console.log(surname);
+// let aircraft = 'VFX'
+// aircraft = 'VFX-1'
+// console.log(aircraft)
+
+const pilot = 'Fokker'
+console.log(pilot)
+
+
+
